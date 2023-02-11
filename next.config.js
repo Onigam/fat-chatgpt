2b402c0b8bf9c9ff3b61268c6d80fdbeef7ff3a5
Magino Marveaux-Cochet <magino.cochet@gmail.com>
2023-02-11 14:43:25 +0100

    fix: increase timeout config to 30s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  server: {
+    // Increase the timeout limit to 30 seconds
+    timeout: 30000,
+  },
 }
 
 module.exports = nextConfig
